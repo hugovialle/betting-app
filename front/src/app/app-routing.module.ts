181c3cd777ab39260507e6149e224e379913d061
Theotime Lameyre <theotime.lameyre@gmail.com>
2021-11-17 10:00:42 +0100

    CSS on login, register, events and navbar

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EventsComponent } from './events/events.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path: 'home', component:HomeComponent },
+  {path: 'login', component:LoginComponent },
+  {path: 'register', component: RegisterComponent },
+  {path: 'events', component: EventsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
